7d9017ceae1fd926bcfdf3982535b74baed04cd6
Raunak Raj <nkray21111983@gmail.com>
2023-07-18 18:07:18 +0530

    v1.1.0

--- readme.md
+++ readme.md
@@ -1,9 +1,69 @@
-## Acode Ayu Mirage Theme
+## Acode Ayu 💫
 
-Acode Ayu Mirage is a plugin designed for the Acode text editor, based on the popular Ayu Mirage VS Code theme. This plugin is perfect for developers who are looking for a sleek and modern look and feel for their code editor.
+Acode Ayu Mirage is a plugin designed for the **Acode editor**, based on the popular **Ayu Mirage VS Code theme**. This plugin is perfect for developers who are looking for a sleek and modern look and feel for their code editor.
+
+<details>
+    <summary><strong>Updates</strong></summary>
+    <br>
+    <details>
+        <summary>
+            <code><strong>v1.1.0</strong></code>
+        </summary>
+        <ul>
+            <li>Improved Themes</li>
+            <li>Added Ayu Mirage Theme</li>
+            <li>Added versatile icon pack</li>
+        </ul>
+    </details>
+</details>
+
+### Features
+
+- Two Beautiful Themes: Acode Ayu Mirage (with a modern touch and dim grayish background) and Ayu Dark (with a modern touch and darker colors).
+- Versatile and Huge Icon Pack: The plugin includes an icon pack of approximately 180+ icons. This icon pack is designed to match the theme and adds icons that are not available in Acode by default.
+
+### Extensive File Icon Collection
+
+Acode Ayu comes with almost every file icon you need. If you find any missing icons, please contribute by submitting a pull request on GitHub or opening an issue. We appreciate your help in making the icon pack even better!
+
+### Warning ⚠️
+
+Please avoid using any other plugin's icon library while using Acode Ayu. There may be conflicts that could override the functionality of this Acode Ayu icon pack.
+
+### How to Use ✨
+
+1. Install the Acode editor if you haven't already.
+2. Install the Acode Ayu plugin from the plugin store(`Acode > Settings > Plugins`).
+3. Follow the instructions in the documentation to enable the desired theme and icon pack.
+4. Enjoy a sleek and modern look and feel for your code editor with Acode Ayu!
 
 > Bug Report - [Click here](https://github.com/bajrangCoder/acode-ayu-mirage-theme)
 
-> Any improvement - [Click here](https://github.com/bajrangCoder/acode-ayu-mirage-theme)
+> Any improvements - [Click here](https://github.com/bajrangCoder/acode-ayu-mirage-theme)
 
-**Hope you all love this theme. ❤️❤️🥺😢**
+## Documentation 📖
+### Enabling/Disabling Icon Pack:
+
+To enable/disable icon follow given steps
+- `Open Acode App > Settings > Plugins`
+- And on plugin page in **installed** plugin section search for Ayu Mirage
+- Open Ayu Mirage Page and on **header top right corner**, there is a ⚙️
+- Press it and it will open plugin setting page and from here you can check/uncheck checkbox to enable/disable icon pack respectively.
+
+### Choosing the Theme: 
+
+For choosing this theme follow given steps:
+- Open `Acode App > Settings > Themes`
+- And Select **App Section** and scroll to bottom
+- there you can get both theme: `Ayu Mirage` and `Ayu Dark`
+- You can select according to your choice
+
+### Contribution 🤝
+
+If you encounter any bugs or have suggestions for improvements, please submit a bug report or enhancement request through the provided links. We appreciate your contributions to make Acode Ayu even better!
+
+### License 🧾
+
+Acode Ayu is released under the [MIT License](https://github.com/bajrangCoder/acode-ayu-mirage-theme/blob/main/LICENSE).
+
+**We hope you all love this theme. ❤️❤️🥺😢**
--- src/main.js
+++ src/main.js
@@ -3,15 +3,82 @@ import style from './style.css'
 
 const themes = acode.require('themes');
 const ThemeBuilder = acode.require('themeBuilder');
+const Url = acode.require("Url");
+const helpers = acode.require("helpers");
+const appSettings = acode.require('settings');
+
+// overiding the acode `helper.getIconForFile()` function to add many more icons
+function getFileType(filename) {
+    const regex = {
+        babel: /\.babelrc$/i,
+        jsmap: /\.js\.map$/i,
+        yarn: /^yarn\.lock$/i,
+        testjs: /\.test\.js$/i,
+        testts: /\.test\.ts$/i,
+        cssmap: /\.css\.map$/i,
+        typescriptdef: /\.d\.ts$/i,
+        clojurescript: /\.cljs$/i,
+        cppheader: /\.(hh|hpp)$/i,
+        jsconfig: /^jsconfig.json$/i,
+        tsconfig: /^tsconfig.json$/i,
+        android: /\.(apk|aab|slim)$/i,
+        jsbeautify: /^\.jsbeautifyrc$/i,
+        webpack: /^webpack\.config\.js$/i,
+        audio: /\.(mp3|wav|ogg|flac|aac)$/i,
+        git: /(^\.gitignore$)|(^\.gitmodules$)/i,
+        video: /\.(mp4|m4a|mov|3gp|wmv|flv|avi)$/i,
+        image: /\.(png|jpg|jpeg|gif|bmp|ico|webp)$/i,
+        npm: /(^package\.json$)|(^package\-lock\.json$)/i,
+        compressed: /\.(zip|rar|7z|tar|gz|gzip|dmg|iso)$/i,
+        eslint: /(^\.eslintrc(\.(json5?|ya?ml|toml))?$|eslint\.config\.(c?js|json)$)/i,
+        postcssconfig: /(^\.postcssrc(\.(json5?|ya?ml|toml))?$|postcss\.config\.(c?js|json)$)/i,
+        prettier: /(^\.prettierrc(\.(json5?|ya?ml|toml))?$|prettier\.config\.(c?js|json)$)/i,
+        font: /\.(ttf|otf|woff|woff2|eot)$/i,
+        replit: /^\.replit$/i, // .replit
+        env: /^\.env$/i, // .env
+        license: /^(license|LICENSE|License)$/i, // License
+        procfile: /^Procfile$/i, // Procfile
+        poetry: /^pyproject\.toml$/i, // Poetry
+        tailwindcss: /tailwind\.config(\.(c?js|json|yaml|yml|ts|tsx))?$|\.tailwindrc(\.(c?js|json|yaml|yml|ts|tsx))?$/i, // Tailwindcss config
+        tauri: /tauri\.config(\.(c?js|json|yaml|yml|ts|tsx))?$/i, // Tauri config
+        vite: /vite\.config(\.(c?js|json|ts|tsx))?$/i // Vite config
+    };
+
+    const fileType = Object.keys(regex).find((type) => regex[type].test(filename));
+    if(fileType) return fileType;
+
+    return Url.extname(filename).substring(1);
+}
+
+helpers.getIconForFile = (filename) => {
+    const { getModeForPath } = ace.require('ace/ext/modelist');
+    const type = getFileType(filename);
+    const { name } = getModeForPath(filename);
+
+    const iconForMode = `file_type_${name}`;
+    const iconForType = `file_type_${type}`;
+
+    return `file file_type_default ${iconForMode} ${iconForType}`;
+}
+
 class AyuTheme {
     #style
-    
+    isIconPack = true;
+    constructor() {
+        if(!appSettings.value[plugin.id]) {
+            appSettings.value[plugin.id] = {
+                iconPack: this.isIconPack,
+            };
+            appSettings.update(false);
+        }
+    }
+
     async init() {
         try {
             this.#style = <style
                 textContent={style}
             ></style>
-            document.head.append(this.#style)
+            document.head.append(this.#style);
             const WHITE = "#FFFFFF";
             // 1st Ayu Mirage theme
             const ayuMirage = new ThemeBuilder('Ayu Mirage', 'dark', 'free');
@@ -32,13 +99,13 @@ class AyuTheme {
             ayuMirage.popupTextColor = WHITE;
             ayuMirage.popupActiveColor = '#FFD173';
             ayuMirage.popupBorderColor = '#63759926';
-            ayuMirage.boxShadowColor = "#12151CB3";
+            ayuMirage.boxShadowColor = "#12151c91";
             ayuMirage.scrollbarColor = "#8A9199CC";
             ayuMirage.buttonActiveColor = "#ffad66c6";
             ayuMirage.buttonBackgroundColor = "#FFAD66";
             ayuMirage.buttonTextColor = WHITE;
             ayuMirage.preferredFont = "Victor Mono Medium";
-            
+
             // 2nd Ayu Mirage Dark version
             const ayuDark = new ThemeBuilder('Ayu Dark', 'dark', 'free');
             ayuDark.primaryColor = '#0F131A';
@@ -66,13 +133,44 @@ class AyuTheme {
             ayuDark.preferredFont = "Victor Mono Medium";
             themes.add(ayuMirage);
             themes.add(ayuDark);
+            if(!this.settings.iconPack){
+                this.#style.remove();
+            }
         } catch(err) {
             acode.alert("Error", err);
         }
     }
+    
+    get settingsObj() {
+        return {
+            list: [
+                {
+                    key: 'iconPack',
+                    text: 'Enable/Disable Icon Pack',
+                    checkbox: this.settings.iconPack,
+                    info: `If set to "true" means checked, then it will add a versatile icon pack to acode and that will match Ayu theme schema. If set to "false" means unchecked then icon pack will not be applied`,
+                },
+            ],
+            cb: (key, value) => {
+                this.settings[key] = value;
+                appSettings.update();
+                if(this.settings.iconPack){
+                    document.head.append(this.#style);
+                } else {
+                    this.#style.remove();
+                }
+            },
+        }
+    }
+    
+    get settings() {
+        return appSettings.value[plugin.id];
+    }
 
     async destroy() {
         this.#style.remove();
+        delete appSettings.value[plugin.id]
+        appSettings.update(false);
     }
 }
 
@@ -86,7 +184,7 @@ if(window.acode) {
         }
         acodePlugin.baseUrl = baseUrl;
         acodePlugin.init($page, cacheFile, cacheFileUrl);
-    });
+    }, acodePlugin.settingsObj);
     acode.setPluginUnmount(plugin.id, () => {
         acodePlugin.destroy();
     });
